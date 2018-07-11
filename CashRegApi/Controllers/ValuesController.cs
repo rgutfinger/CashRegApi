df9c8ee5c484cd332d2f67573ce72e99a932f87d
rgutfinger <gutfinger@cox.net>
2018-07-11 14:53:43 -0700

    put - discount

--- CashRegApi/Controllers/ValuesController.cs
+++ CashRegApi/Controllers/ValuesController.cs
@@ -40,7 +40,7 @@ namespace CashRegApi.Controllers
 
 
 		// PUT api/values/5
-		public void Put(int id, [FromBody]string value)
+		public void PutDiscountByPercent([FromBody]double percent)
 		{
 		}
 
